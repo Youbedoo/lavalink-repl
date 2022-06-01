b56de13f12be1dbc585c669ad3756935c29a2c5e
Atharv Pathak <78468172+Atharv-Pathak-14@users.noreply.github.com>
2022-06-01 18:06:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const { createWriteStream } = require("fs");
 const { execSync } = require("child_process");
 
 const url = "https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev";
-
+const url2 = "https://github.com/shyiko/jabba/raw/master/install.sh"
 const start = () => {
-    const download = stream(url).pipe(createWriteStream('Lavalink.jar'));
+    const download = stream(url2).pipe(createWriteStream('jabba.sh'));
     download.on("finish", () => {
         execSync("java -jar Lavalink.jar", { stdio: "inherit" });
     });
